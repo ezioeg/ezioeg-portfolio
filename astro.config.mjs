bf1c6164a445e382d14b0d892229c62dc6655f6a
ezioeg <ezioeg@gmail.com>
2025-05-30 10:22:59 -0400

    chore: initial commit with base project structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
     output: "static",
-    site: "https://ezioeg-portfolio.vercel.app",
+    adapter: vercel(),
 });
